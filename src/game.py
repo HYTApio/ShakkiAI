a5925b634938c2cb8dffe564b75133d174d4a1f0
HYTapio <tapio.uzal@helsinki.fi>
2022-11-09 14:29:30 +0200

    created game.py

---
+++ src/game.py
@@ -0,0 +1,39 @@
+import chess
+from checkmate import is_check
+
+
+def play():
+    board = chess.Board()
+    while True:
+        print(board)
+        if is_check(board):
+            print("shakki")
+            break
+
+        if board.turn == chess.WHITE:
+            print("valkoisen vuoro")
+            while True:
+                siirto = input("mitä siirretään? ")
+                if siirto == "":
+                    return
+                if chess.Move.from_uci(siirto) not in board.legal_moves:
+                    print("väärä siirto kokeile uudestaan")
+                else:
+                    break
+            board.push(chess.Move.from_uci(siirto))
+
+        else:
+            print("mustan vuoro")
+            while True:
+                siirto = input("mitä siirretään? ")
+                if siirto == "":
+                    return
+                if chess.Move.from_uci(siirto) not in board.legal_moves:
+                    print("väärä siirto kokeile uudestaan")
+                else:
+                    break
+            board.push(chess.Move.from_uci(siirto))
+
+    
+if __name__=="__main__":
+    play()
