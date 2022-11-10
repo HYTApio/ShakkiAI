784d3d2aea1f832dd896b117f0b5d4cca19f680d
HYTapio <tapio.uzal@helsinki.fi>
2022-11-10 14:50:30 +0200

    added new tests

--- src/tests/chechmate_test.py
+++ src/tests/chechmate_test.py
@@ -8,3 +8,75 @@ class TestCheckmate(unittest.TestCase):
     
     def test_beginning_not_check(self):
         self.assertEqual(checkmate.is_check(self.board), False)
+    
+    def test_check_from_above(self):
+        board = chess.Board('3qk3/8/8/8/8/8/8/3K4 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), True)
+        board = chess.Board('3rk3/8/8/8/8/8/8/3K4 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), True)
+        board = chess.Board('4k3/8/8/8/8/8/8/3K4 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), False)
+
+    def test_check_from_below(self):
+        board = chess.Board('4K3/8/8/8/8/8/8/3kq3 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), True)
+        board = chess.Board('4K3/8/8/8/8/8/8/3kr3 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), True)
+        board = chess.Board('4K3/8/8/8/8/8/8/4k3 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), False)
+
+    def test_check_from_left(self):
+        board = chess.Board('1q1K4/8/8/8/8/8/8/3k4 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), True)
+        board = chess.Board('1r1K4/8/8/8/8/8/8/3k4 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), True)
+        board = chess.Board('3k4/8/8/8/8/8/8/3K4 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), False)
+
+    def test_check_from_right(self):
+        board = chess.Board('4K1q1/8/8/8/8/8/8/3k4 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), True)
+        board = chess.Board('4K1r1/8/8/8/8/8/8/3k4 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), True)
+        board = chess.Board('4k3/8/8/8/8/8/8/3K4 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), False)
+
+    def test_check_from_diag_above(self):
+        board = chess.Board('8/5p2/4K3/8/8/8/8/3k4 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), True)
+        board = chess.Board('8/3p4/4K3/8/8/8/8/3k4 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), True)
+        board = chess.Board('8/8/8/2q5/8/4K3/8/3k4 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), True)
+        board = chess.Board('8/8/8/6q1/8/4K3/8/3k4 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), True)
+        board = chess.Board('8/8/2b5/8/4K3/8/8/3k4 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), True)
+        board = chess.Board('8/8/6b1/8/4K3/8/8/3k4 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), True)
+
+    def test_check_from_diag_below(self):
+        board = chess.Board('8/8/4k3/8/5P2/8/8/3K4 w KQkq - 0 1')
+        board.push(chess.Move.from_uci("f4f5"))
+        self.assertEqual(checkmate.is_check(board), True)
+        board = chess.Board('8/8/4k3/8/3P4/8/8/3K4 w KQkq - 0 1')
+        board.push(chess.Move.from_uci("d4d5"))
+        self.assertEqual(checkmate.is_check(board), True)
+        board = chess.Board('8/4K3/8/2q5/8/8/8/3k4 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), True)
+        board = chess.Board('8/4K3/8/6q1/8/8/8/3k4 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), True)
+        board = chess.Board('4K3/8/2b5/8/8/8/8/3k4 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), True)
+        board = chess.Board('4K3/8/6b1/8/8/8/8/3k4 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), True)
+        board = chess.Board('4K3/5P2/6b1/8/8/8/8/3k4 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), False)
+        board = chess.Board('4K3/3P4/2b5/8/8/8/8/3k4 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), False)
+
+
+    def test_check_knight(self):
+        board = chess.Board('k7/8/8/2n5/4K3/8/8/8 w KQkq - 0 1')
+        self.assertEqual(checkmate.is_check(board), True)
+
