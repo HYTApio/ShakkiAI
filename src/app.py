5519a6738465502de34ddf8dd5fea542b9981b46
HYTapio <tapio.uzal@helsinki.fi>
2022-12-17 20:25:36 +0200

    lisätty graafinen liittymä

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 """ Module providing chess game base"""
 import chess
+
+from chessboard import display
 from checks.checkmate import possible_moves as checkmate
 from checks.check import is_check
 from ai.minmax import find_best_move
@@ -11,8 +13,11 @@ def play():
     """
     board = chess.Board()
     last_move = None
+    game_board = display.start()
     while True:
-        print(board)
+        #print(board)
+        display.check_for_quit()
+        display.update(board.fen(), game_board)
         #checks if the game is in check / checkmate
         check = is_check(board, last_move)
         if check[0]:
@@ -30,16 +35,17 @@ def play():
         else:
             print("mustan vuoro")
             while True:
-                move = input("mitä siirretään? ")
-                if move == "":
+                move = input("mitä siirretään (kirjoita poistu lopettaaksesi)? ")
+                if move == "poistu":
                     return
                 try:
                     if chess.Move.from_uci(move) not in board.legal_moves:
                         print("väärä siirto kokeile uudestaan")
                     else:
                         break
-                except:
+                except ValueError:
                     print("väärä siirto kokeile uudestaan")
 
             board.push(chess.Move.from_uci(move))
             last_move = chess.Move.from_uci(move)
+    display.terminate()
